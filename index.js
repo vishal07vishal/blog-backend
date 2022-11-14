b949c9718ca8d065eda44c9d6ad3be5266d0a225
Vishal Masal <vishalmasal557@gmail.com>
2022-11-14 11:32:02 +0530

    updates

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
-const express=require ('express')
-const app=express();
+const express=require('express')
+const app=express()
+const data1=require("./Routes/bollywood")
 
-const middleware1=(req,res,next)=>{
-    console.log("midleware1")
-    next()
-}
+app.use("/home",data1)
 
-const middleware2=(req,res,next)=>{
-    console.log("midleware 2")
-    next()
-}
-app.use(middleware2)
-
-app.get('/link1',middleware1,(req,res)=>{
-    res.send("<h1>link with middleware 1 and 2 <h1>")
-})
-
-app.get('/link2',(req,res)=>{
-    res.send("<h1>link2 with middleware2 <h1>")
-})
-app.get('/link3',(req,res)=>{
-    res.send("<h1>link3 with middleware2 <h1>")
-})
-
-
-app.get('/link4',middleware1,(req,res)=>{
-    res.send("<h1>link4 with middleware 1 and 2 <h1>")
-})
-
-app.listen(5000,()=>{
-    console.log("server started")
+app.listen(process.env.PORT||8080,()=>{
+    console.log("server is running")
 })
